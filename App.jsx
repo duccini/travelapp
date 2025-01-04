c29ac2fcb9b643fd41822385301255921eb83624
duccini <guilhermeduccini@gmail.com>
2025-01-04 11:01:28 -0300

    config: add navigation

--- App.jsx
+++ App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
-
 import {StyleSheet} from 'react-native';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
 import Home from './src/screens/Home';
+import AttractionDetails from './src/screens/AttractionDetails';
+import Gallery from './src/screens/Gallery';
 
 /**
  * SafeAreaView start the View from the Safe part - iOS +10 & other devices
@@ -10,8 +15,18 @@ import Home from './src/screens/Home';
  * style, also check for errors
  */
 
+const Stack = createNativeStackNavigator();
+
 function App() {
-  return <Home />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen component={Home} name="Home" />
+        <Stack.Screen component={AttractionDetails} name="AttractionDetails" />
+        <Stack.Screen component={Gallery} name="Gallery" />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({});
---
+++ src/screens/AttractionDetails/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const AttractionDetails = () => {
+  return (
+    <SafeAreaView>
+      <Text>Attraction Details</Text>
+    </SafeAreaView>
+  );
+};
+
+export default React.memo(AttractionDetails);
